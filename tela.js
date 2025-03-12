60f9ddb86de2202ef450fb422048c89cffd06c52
pedro lucas <160865525+Pedroifpi@users.noreply.github.com>
2025-03-12 07:15:19 -0300

    Delete tela.js

--- tela.js
+++
@@ -1,6 +0,0 @@
-function AdicionarHorario(dia) {
-    const container = document.querySelector(`.dia:has(span:contains(${dia}))`);
-    const input = document.createElement('input');
-    input.type = 'time';
-    container.appendChild(input);
-}
